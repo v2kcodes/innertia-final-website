4afbd43d8ea3e968f4403b8ca2f752eff5e021a6
Innertia Software Solutions <innertiass@gmail.com>
2025-09-15 19:19:16 +0200

    fix: Simplify ThemeProvider with hardcoded props
    Use literal values instead of variables to avoid type issues

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -5,27 +5,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 interface ThemeProviderProps {
   children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
 }
 
-export function ThemeProvider({
-  children,
-  attribute = "class",
-  defaultTheme = "system",
-  enableSystem = true,
-  disableTransitionOnChange = true,
-  ...props
-}: ThemeProviderProps) {
+export function ThemeProvider({ children }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute={attribute}
-      defaultTheme={defaultTheme}
-      enableSystem={enableSystem}
-      disableTransitionOnChange={disableTransitionOnChange}
-      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
